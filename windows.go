1e9933389ce1f9ad5ddb84f8e8dc1c415d5deeca
Manuel Laggner <manuel.laggner@gmail.com>
2023-08-06 14:05:35 +0200

    windows - use wchar_t

--- windows.go
+++ windows.go
@@ -57,14 +57,14 @@ func jni_CreateJavaVM(pvm unsafe.Pointer, penv unsafe.Pointer, args unsafe.Point
 func LoadJVMLib(jvmLibPath string) error {
 	cs := cString(jvmLibPath)
 	defer free(cs)
-	libHandle := C.LoadLibrary((*C.char)(cs))
+	libHandle := C.LoadLibrary((*C.wchar_t)(cs))
 	if libHandle == nil {
 		return errors.New("could not dynamically load jvm.dll")
 	}
 
 	cs2 := cString("JNI_GetDefaultJavaVMInitArgs")
 	defer free(cs2)
-	ptr := C.GetProcAddress(libHandle, (*C.char)(cs2))
+	ptr := C.GetProcAddress(libHandle, (*C.wchar_t)(cs2))
 	if ptr == nil {
 		return errors.New("could not find JNI_GetDefaultJavaVMInitArgs in jvm.dll")
 	}
@@ -72,7 +72,7 @@ func LoadJVMLib(jvmLibPath string) error {
 
 	cs3 := cString("JNI_CreateJavaVM")
 	defer free(cs3)
-	ptr = C.GetProcAddress(libHandle, (*C.char)(cs3))
+	ptr = C.GetProcAddress(libHandle, (*C.wchar_t)(cs3))
 	if ptr == nil {
 		return errors.New("could not find JNI_CreateJavaVM in jvm.dll")
 	}
